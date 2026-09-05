80f6f2336d51417f6973d95cf72765d1fa7c5631
YSJ2576011071 <jbjbhj123@gmail.com>
2026-09-05 23:41:13 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer;
+    int max = -1;
+    int index = 0;
+    for (int i = 0; i < array.size() ; i++) {
+        if (array[i] > max) {
+            max = array[i];
+            index = i;
+        }
+    }
+    answer.push_back(max);
+    answer.push_back(index);
+    return answer;
+}
