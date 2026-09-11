55c7d99f4fa8ecd991e25a47618e7bded2ff38df
Seongjun Yoon <jbjbhj123@gmail.com>
2026-09-11 18:13:59 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if (a < b) {
+        for (a; a <= b ; a++) {
+            answer += a;
+        }
+    } else {
+        for (b; b <= a ; b++) {
+            answer += b;
+        }
+    }
+    return answer;
+}
