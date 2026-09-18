1aef95f8a5ecb48ef69558976199e967ae28a501
Seongjun Yoon <jbjbhj123@gmail.com>
2026-09-18 12:08:38 +0900

    [level 1] Title: 행렬의 덧셈, Time: 39.92 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    int row = arr1.size();
+    int col = arr1[0].size();
+    vector<vector<int>> answer(row, vector<int>(col, 0));
+    
+    for (int i = 0 ; i < arr1.size() ; i++) {
+        for (int j = 0 ; j < arr1[i].size(); j++) {
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+    return answer;
+}
