917db11e4529d3bbf2be732f81389987d2de3159
Seongjun Yoon <jbjbhj123@gmail.com>
2026-09-18 12:09:51 +0900

    Update 행렬의 덧셈.cpp

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -6,6 +6,7 @@ using namespace std;
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     int row = arr1.size();
     int col = arr1[0].size();
+    // 미리 얼만큼 벡터를 사요할 건지 명시해줘야 함
     vector<vector<int>> answer(row, vector<int>(col, 0));
     
     for (int i = 0 ; i < arr1.size() ; i++) {
